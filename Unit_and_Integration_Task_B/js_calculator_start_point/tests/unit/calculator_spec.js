1a593a727c4de7c37bde001dc190009c8cc299a0
sarahmurphy86 <sarah.graham86@gmail.com>
2018-07-24 16:19:36 +0100

    tests amended in task B

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -9,46 +9,42 @@ describe('calculator', function () {
 // calculator.add()
 	// add 1+4 and get 5
   it('it should add the numbers one and four together and return five', function(){
-    // calculator.numberClick(1);
-    // calculator.operatorClick('+');
-    // calculator.numberClick(4);
-    // calculator.operatorClick('=');
-    // assert.strictEqual(5, calculator.runningTotal)
-    assert.strictEqual(5, 4+1)
+    calculator.numberClick(1);
+    calculator.operatorClick('+');
+    calculator.numberClick(4);
+    calculator.operatorClick('=');
+    assert.strictEqual(5, calculator.runningTotal)
   });
 
 	// calculator.subtract()
   // subtract 7-4 and get 3
   it('it should subtract the number four from seven and return three', function(){
-    // calculator.numberClick(7);
-    // calculator.operatorClick('-');
-    // calculator.numberClick(4);
-    // calculator.operatorClick('=');
-    // assert.strictEqual(3, calculator.runningTotal)
-    assert.strictEqual(3, 7-4)
+    calculator.numberClick(7);
+    calculator.operatorClick('-');
+    calculator.numberClick(4);
+    calculator.operatorClick('=');
+    assert.strictEqual(3, calculator.runningTotal)
   });
 
   // calculator.multiply()
   // multiply 3x5 and get 15
   it('it should multiply the numbers three and five and return fifteen', function(){
-    // calculator.numberClick(3);
-    // calculator.operatorClick('*');
-    // calculator.numberClick(5);
-    // calculator.operatorClick('=');
-    // assert.strictEqual(15, calculator.runningTotal)
-    assert.strictEqual(15, 3*5)
+    calculator.numberClick(3);
+    calculator.operatorClick('*');
+    calculator.numberClick(5);
+    calculator.operatorClick('=');
+    assert.strictEqual(15, calculator.runningTotal)
   });
 
   // calculator.divide()
   // divide 21/7 and get 3
   it('it should divide 21 by seven and return three', function(){
-    // calculator.numberClick(2);
-    // calculator.numberClick(1);
-    // calculator.operatorClick('/');
-    // calculator.numberClick(7);
-    // calculator.operatorClick('=');
-    // assert.strictEqual(3, calculator.runningTotal)
-    assert.strictEqual(3, 21/7)
+    calculator.numberClick(2);
+    calculator.numberClick(1);
+    calculator.operatorClick('/');
+    calculator.numberClick(7);
+    calculator.operatorClick('=');
+    assert.strictEqual(3, calculator.runningTotal)
   });
 
   // calculator.numberClick()
